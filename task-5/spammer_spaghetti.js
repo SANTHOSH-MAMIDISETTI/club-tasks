41f3b76dbbac5e245e8b95b95b1be04c6307806c
SANTHOSH-MAMIDISETTI <mamidisettisanthosh2004@gamil.com>
2023-01-16 16:40:37 +0530

    file

--- task-5/spammer_spaghetti.js
+++ task-5/spammer_spaghetti.js
@@ -1,18 +1,33 @@
-const fetchData = async () => {
-    const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-    const myJson = await response.json(); 
-    const setup = myJson.setup;
-    const punchline = myJson.punchline;
-    return { setup, punchline }
-}
+import puppeteer from 'puppeteer';
 
-const sendData = (data) => {
-    document.getElementsByClassName('input-message-input')[0].innerHTML = data
-    document.querySelector('.btn-send').click();	
-}
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-let timer = setInterval(async function() {
-    let data = await fetchData()
-    sendData(data.setup)
-    sendData(data.punchline)
-}, 10000);
+  await page.goto('https://developers.google.com/web/');
+
+  // Type into search box.
+  await page.type('.devsite-search-field', 'Headless Chrome');
+
+  // Wait for suggest overlay to appear and click "show all results".
+  const allResultsSelector = '.devsite-suggest-all-results';
+  await page.waitForSelector(allResultsSelector);
+  await page.click(allResultsSelector);
+
+  // Wait for the results page to load and display the results.
+  const resultsSelector = '.gsc-results .gs-title';
+  await page.waitForSelector(resultsSelector);
+
+  // Extract the results from the page.
+  const links = await page.evaluate(resultsSelector => {
+    return [...document.querySelectorAll(resultsSelector)].map(anchor => {
+      const title = anchor.textContent.split('|')[0].trim();
+      return `${title} - ${anchor.href}`;
+    });
+  }, resultsSelector);
+
+  // Print all the files.
+  console.log(links.join('\n'));
+
+  await browser.close();
+})();
