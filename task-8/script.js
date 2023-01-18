aca6ab2a09f76dc3eeb9942f44dfdb07d236cb61
SANTHOSH-MAMIDISETTI <mamidisettisanthosh2004@gamil.com>
2023-01-18 07:16:19 +0530

    TASK -8

--- task-8/script.js
+++ task-8/script.js
@@ -1,10 +1,55 @@
-fetch('https://pokeapi.co/api/v2/pokemon')
-  .then(response => response.json())
-  .then(data => {
-    // Do something with the data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error(error);
-  });
+const poke_container = document.getElementById('poke_container');
+const pokemons_number = prompt('How many pokemons do you want to see?');
+
+if (pokemons_number <1) 
+{
+  alert('There are only 151 pokemons');
+  const pokemons_number = prompt('How many pokemons do you want to see?');
+
+}
+else if (pokemons_number > 1 || pokemons_number <=151) 
+{
+
+const fetchPokemons = async () => 
+{
+	for (let i = 1; i <= pokemons_number; i++) 
+  {
+		await getPokemon(i);
+	}
+};
+
+const getPokemon = async id => 
+{
+  const url =`https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  createPokemonCard(pokemon);
+}
+
+const createPokemonCard = (pokemon) => 
+{
+  const pokemonEl = document.createElement('div');
+  pokemonEl.classList.add('pokemon');
+  const { id, name, sprites, types } = pokemon;
+  const type = types[0].type.name;
+  const pokeInnerHTML = `
+  <div class="img-container">
+    <img src="${sprites.front_default}" alt="${name}" />
+  </div>
+  <div class="info">
+    <span class="number">${id}</span>
+    <h3 class="name">${name}</h3>
+    <small class="type">Type: <span>${type}</span></small>
+  </div>
+  `;
+  pokemonEl.innerHTML = pokeInnerHTML;
+	poke_container.appendChild(pokemonEl);
+}
+
+fetchPokemons();
+}
+else 
+{
+  alert('Please enter a number');
+  const pokemons_number = prompt('How many pokemons do you want to see?');
+}
